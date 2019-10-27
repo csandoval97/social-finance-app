9f0f4e15d945ba5c609f36478beac5f10d36fbe1
KhiemTruong23 <ktruong2@hawk.iit.edu>
2019-10-26 23:08:54 -0500

    new UI

--- financeFront/src/app/app.module.ts
+++ financeFront/src/app/app.module.ts
@@ -4,15 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { ChartsModule } from 'ng2-charts';
+import { Routes, RouterModule } from '@angular/router';
+import { MyPieChartComponent } from './my-pie-chart/my-pie-chart.component';
+import { SavingComponent } from './saving/saving.component';
+import { CreditComponent } from './credit/credit.component';
+import { LoansComponent } from './loans/loans.component';
+import { PurchasesComponent } from './purchases/purchases.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MyPieChartComponent,
+    SavingComponent,
+    CreditComponent,
+    LoansComponent,
+    PurchasesComponent
   ],
+  
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
